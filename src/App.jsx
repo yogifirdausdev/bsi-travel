80fa3066a676402d9bc542fbc4c7cdca3e2cb24b
Yogi Firdaus <35126211+yogifirdausdev@users.noreply.github.com>
2023-10-08 04:17:07 +0700

    First-setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-blue-700 text-8xl">abcdefghijklmnopqrstuvwxyz</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans'],
+      volkhov: ['volkhov'],
+    },
+    colors: {
+      'my-primary': '#F1A501',
+      'my-secondary': '#DF6951',
+      'my-purple': '#6246E5',
+      'my-heading': '#181E4B',
+      'my-paragraph': '#5E6282',
+    },
+  },
+  plugins: [],
+}
