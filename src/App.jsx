f2f8a181922038d91e9517704b1a07eb8ec39be6
Yogi Firdaus <35126211+yogifirdausdev@users.noreply.github.com>
2023-10-10 14:00:18 +0700

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 <<<<<<< HEAD
+<<<<<<< HEAD
 import Hero from './components/Hero'
 import CategorySection from './components/CategorySection'
 =======
@@ -15,6 +16,15 @@ function App() {
     <div className="h-[10000px]">
       <Hero />
       <CategorySection />
+=======
+
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-blue-700 text-8xl">
+        Hello <span>World!</span>
+      </h1>
+>>>>>>> parent of 65a06c4 (Navbar done)
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'my-primary': '#F1A501',
-        'my-secondary': '#DF6951',
-        'my-purple': '#6246E5',
-        'my-heading': '#181E4B',
-        'my-paragraph': '#5E6282',
-      },
-      fontFamily: {
-        sans: ['Poppins', 'sans'],
-        volkhov: ['volkhov'],
-      },
+    extend: {},
+    fontFamily: {
+      sans: ['Poppins', 'sans'],
+      volkhov: ['volkhov'],
+    },
+    colors: {
+      'my-primary': '#F1A501',
+      'my-secondary': '#DF6951',
+      'my-purple': '#6246E5',
+      'my-heading': '#181E4B',
+      'my-paragraph': '#5E6282',
     },
   },
   plugins: [],
